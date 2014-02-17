0340e4a4b7bcbbd6ba44022b4f425b621c9b359b
aidankmcl <aidan.mclaughlin@students.olin.edu>
2014-02-17 01:19:56 -0500

    first round

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,8 +8,10 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
+#define MAX_MATCHES 1
 
 char tracks[][80] = {
     "So What",
@@ -36,9 +38,21 @@ void find_track(char search_for[])
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
-void find_track_regex(char pattern[])
-{
+                    //char pattern[]
+void find_track_regex(regex_t *pexp) {
     // TODO: fill this in
+    int i;
+    regmatch_t matches[MAX_MATCHES]; //A list of the matches in the string (a list of 1)
+    //Compare the string to the expression
+    //regexec() returns 0 on match, otherwise REG_NOMATCH
+    for (i=0;i<NUM_TRACKS;i++) {
+        if (regexec(pexp, tracks[i], MAX_MATCHES, matches, 0) == 0) {
+            printf("Track \"%i\" : \"%s\" \n",i, tracks[i]);
+        }
+        // } else {
+        //     printf("\"%s\" does not match\n", tracks[i]);
+        // }
+    }
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -53,14 +67,21 @@ void rstrip(char s[])
 int main (int argc, char *argv[])
 {
     char search_for[80];
+    int rv;
+    regex_t exp; //Our compiled expression
 
-    /* take input from the user and search */
-    printf("Search for: ");
-    fgets(search_for, 80, stdin);
+    printf("Search with what pattern: ");
+    fgets(search_for,80,stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    rv = regcomp(&exp, search_for, REG_EXTENDED);
+    if (rv != 0) {
+        printf("regcomp failed with %d\n", rv);
+    }
 
+    /* take input from the user and search */
+    find_track_regex(&exp);
+
+    regfree(&exp);
     return 0;
 }
