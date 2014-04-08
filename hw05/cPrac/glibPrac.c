e1737cdeeae4fd7202b283cd4915a5403abb939f
aidankmcl <aidan.mclaughlin@students.olin.edu>
2014-04-07 20:32:17 -0400

    working basic

--- hw05/cPrac/glibPrac.c
+++ hw05/cPrac/glibPrac.c
@@ -2,38 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 void parser(FILE *file, GHashTable *hash) {
-
-	printf("The capital of Texas is %i\n", g_hash_table_lookup(hash, "Texas"));
-	g_hash_table_insert(hash, "Woof", 2);
-
 	char word[30];
 
 	while (fscanf(file,"%s",word) != EOF) {
 		int value = g_hash_table_lookup(hash, word);
+
+		char *toAdd = malloc(30);
+		strcpy(toAdd,word);
+
 		if (value == NULL) {
-			puts("added!");
-			printf("word: %s, value: %i\n\n",word,value);
-			g_hash_table_insert(hash, word, 1);
+			g_hash_table_insert(hash, toAdd, 1);
 		} else {
-			puts("else happened");
-			printf("word: %s, value: %i\n\n",word,value);
-			int value = g_hash_table_lookup(hash, word);
+			int value = g_hash_table_lookup(hash, toAdd);
 			value++;
-			g_hash_table_replace(hash, word, value);
+			g_hash_table_replace(hash, toAdd, value);
 		}
 	}
 }
 
+static void spit(gpointer key, gpointer value, gpointer userdata) {
+    printf("word: %s, freq: %i\n",key,value);
+}
+
 int main() {
 	FILE *file = fopen("metaKafka.txt","r");
-
 	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
-
-	printf("There are %d keys in the hash\n", g_hash_table_size(hash));
 	parser(file, hash);
-	sleep(1);
-	printf("How many times does is appear: %i\n", g_hash_table_lookup(hash, "is"));
 
+	g_hash_table_foreach(hash, spit, NULL);
 	return 0;
 }
